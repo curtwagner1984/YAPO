26f4d8398bad56f860dfaad565adce2a9affd5d1
Curt Wagner <curtwagner1984@gmail.com>
2016-09-12 16:34:05 +0300

    fix: implemented angular material inf scroll in actor list view, all the html is just a placeholder, my goal was to make it functional, not to make it pretty.     !minor

--- videos/static/js/app/actor-list/actor-list.component.js
+++ videos/static/js/app/actor-list/actor-list.component.js
@@ -24,8 +24,8 @@ angular.module('actorList').component('actorList', {
 
     }],
 
-    controller: ['$scope', 'Actor', 'pagerService', 'Scene', 'ActorTag', 'scopeWatchService', 'helperService',
-        function ActorListController($scope, Actor, pagerService, Scene, ActorTag, scopeWatchService, helperService) {
+    controller: ['$scope', 'Actor', 'pagerService', 'Scene', 'ActorTag', 'scopeWatchService', 'helperService', '$timeout',
+        function ActorListController($scope, Actor, pagerService, Scene, ActorTag, scopeWatchService, helperService, $timeout) {
 
 
             var self = this;
@@ -33,6 +33,143 @@ angular.module('actorList').component('actorList', {
             var didSceneLoad = false;
             var didActorTagLoad = false;
             var didSectionListWrapperLoad = false;
+
+
+            // AM deferred loading wrapper https://material.angularjs.org/latest/demo/virtualRepeat
+
+            // In this example, we set up our model using a class.
+            // Using a plain object works too. All that matters
+            // is that we implement getItemAtIndex and getLength.
+            var DynamicItems = function () {
+                /**
+                 * @type {!Object<?Array>} Data pages, keyed by page number (0-index).
+                 */
+                this.loadedPages = {};
+
+                /** @type {number} Total number of items. */
+                this.numItems = 0;
+
+                /** @const {number} Number of items to fetch per request. */
+                if (helperService.getNumberOfItemsPerPaige() != undefined) {
+                    this.PAGE_SIZE = helperService.getNumberOfItemsPerPaige();
+                } else {
+                    this.PAGE_SIZE = 10;
+                }
+
+
+                this.fetchNumItems_();
+            };
+
+
+            DynamicItems.prototype.reset = function () {
+                this.loadedPages = {};
+                this.numItems = 0;
+
+            };
+
+            // Required.
+            DynamicItems.prototype.getItemAtIndex = function (index) {
+                var pageNumber = Math.floor(index / this.PAGE_SIZE);
+                var page = this.loadedPages[pageNumber];
+
+                if (page) {
+                    return page[index % this.PAGE_SIZE];
+                } else if (page !== null) {
+                    this.fetchPage_(pageNumber);
+                }
+            };
+
+            // Required.
+            DynamicItems.prototype.getLength = function () {
+                return this.numItems;
+            };
+
+            DynamicItems.prototype.nextPage = function (pageNumber, wasCalledFromDynamicItems) {
+
+                var loadedPages = this.loadedPages;
+
+                var input = {
+                    currentPage: pageNumber,
+                    pageType: self.pageType,
+                    scene: self.scene,
+                    searchTerm: self.searchTerm,
+                    searchField: self.searchField,
+                    sortBy: self.sortBy,
+                    actorTag: self.actorTag,
+                    isRunnerUp: self.runnerUp
+
+
+                };
+
+                self.actorsToadd = pagerService.getNextPage(input);
+                if (self.actorsToadd != undefined) {
+                    self.actorsToadd.$promise.then(function (res) {
+
+                        // self.actorsToadd = res[0];
+
+                        var paginationInfo = {
+                            pageType: input.pageType,
+                            pageInfo: res[1]
+                        };
+
+                        self.totalItems = parseInt(paginationInfo.pageInfo.replace(/.*<(\d+)>; rel="count".*/, '$1'));
+
+                        scopeWatchService.paginationInit(paginationInfo);
+
+                        self.websites = helperService.resourceToArray(res[0]);
+
+                        if (wasCalledFromDynamicItems) {
+                            for (var i = 0; i < self.websites.length; i++) {
+                                loadedPages[pageNumber].push(self.websites[i])
+                            }
+
+                            this.loadedPages = loadedPages;
+                        } else {
+                            if (self.totalItems == -6) {
+                                self.totalItems = self.websites.length;
+                            }
+
+                            self.dynamicItems.numItems = self.totalItems;
+
+                        }
+
+
+                    });
+                }
+
+            };
+
+            DynamicItems.prototype.fetchPage_ = function (pageNumber) {
+                // Set the page to null so we know it is already being fetched.
+                this.loadedPages[pageNumber] = null;
+
+                // For demo purposes, we simulate loading more items with a timed
+                // promise. In real code, this function would likely contain an
+                // $http request.
+                $timeout(angular.noop, 300).then(angular.bind(this, function () {
+                    this.loadedPages[pageNumber] = [];
+
+                    this.nextPage(pageNumber, true);
+
+
+                    // var pageOffset = pageNumber * this.PAGE_SIZE;
+                    // for (var i = pageOffset; i < pageOffset + this.PAGE_SIZE; i++) {
+                    //   this.loadedPages[pageNumber].push(i);
+                    // }
+                }));
+            };
+
+            DynamicItems.prototype.fetchNumItems_ = function () {
+                // For demo purposes, we simulate loading the item count with a timed
+                // promise. In real code, this function would likely contain an
+                // $http request.
+
+                $timeout(angular.noop, 500).then(angular.bind(this, function () {
+                    this.numItems = self.totalItems;
+                }));
+            };
+
+            this.dynamicItems = new DynamicItems();
 
 
             self.actors = [];
@@ -57,42 +194,42 @@ angular.module('actorList').component('actorList', {
             });
 
 
-            self.nextPage = function (currentPage) {
-
-
-                input = {
-                    currentPage: currentPage,
-                    pageType: self.pageType,
-                    actorTag: self.actorTag,
-                    scene: self.scene,
-                    searchTerm: self.searchTerm,
-                    searchField: self.searchField,
-                    sortBy: self.sortBy,
-                    isRunnerUp: self.runnerUp
-
-                };
-
-
-                self.actorsToadd = pagerService.getNextPage(input);
-
-                self.actorsToadd.$promise.then(function (res) {
-
-                    // self.actorsToadd = res[0];
-
-                    var paginationInfo = {
-                        pageType: input.pageType,
-                        pageInfo: res[1]
-                    };
-
-                    scopeWatchService.paginationInit(paginationInfo);
-
-                    self.actors = helperService.resourceToArray(res[0]);
-
-
-                });
-
-
-            };
+            // self.nextPage = function (currentPage) {
+            //
+            //
+            //     input = {
+            //         currentPage: currentPage,
+            //         pageType: self.pageType,
+            //         actorTag: self.actorTag,
+            //         scene: self.scene,
+            //         searchTerm: self.searchTerm,
+            //         searchField: self.searchField,
+            //         sortBy: self.sortBy,
+            //         isRunnerUp: self.runnerUp
+            //
+            //     };
+            //
+            //
+            //     self.actorsToadd = pagerService.getNextPage(input);
+            //
+            //     self.actorsToadd.$promise.then(function (res) {
+            //
+            //         // self.actorsToadd = res[0];
+            //
+            //         var paginationInfo = {
+            //             pageType: input.pageType,
+            //             pageInfo: res[1]
+            //         };
+            //
+            //         scopeWatchService.paginationInit(paginationInfo);
+            //
+            //         self.actors = helperService.resourceToArray(res[0]);
+            //
+            //
+            //     });
+            //
+            //
+            // };
 
             // if (self.mainPage) {
             //     console.log("main page is true! + " + self.mainPage);
@@ -101,7 +238,7 @@ angular.module('actorList').component('actorList', {
 
             $scope.$on("paginationChange", function (event, pageInfo) {
                 if (pageInfo.pageType == self.pageType) {
-                    self.nextPage(pageInfo.page)
+                    // self.nextPage(pageInfo.page)
                 }
             });
 
@@ -112,7 +249,10 @@ angular.module('actorList').component('actorList', {
             $scope.$on("actorTagLoaded", function (event, loadedActorTag) {
                 self.actors = [];
                 self.actorTag = loadedActorTag;
-                self.nextPage(0);
+                self.dynamicItems.reset();
+                self.dynamicItems.nextPage(0, false);
+
+                // self.nextPage(0);
                 didActorTagLoad = true
 
             });
@@ -122,7 +262,9 @@ angular.module('actorList').component('actorList', {
 
                 self.actors = [];
                 self.scene = scene;
-                self.nextPage(0);
+                // self.nextPage(0);
+                self.dynamicItems.reset();
+                self.dynamicItems.nextPage(0, false);
                 didSceneLoad = true;
 
             });
@@ -132,9 +274,11 @@ angular.module('actorList').component('actorList', {
                     console.log("Sort Order Changed!");
                     self.actors = [];
                     self.sortBy = sortOrder['sortBy'];
-                    
-                    if (sortOrder.mainPage == undefined || sortOrder.mainPage == true ) {
-                        self.nextPage(0);
+                    self.dynamicItems.reset();
+
+                    if (sortOrder.mainPage == undefined || sortOrder.mainPage == true) {
+                        // self.nextPage(0);
+                        self.dynamicItems.nextPage(0, false)
                     }
                     didSectionListWrapperLoad = true;
                 }
@@ -157,9 +301,11 @@ angular.module('actorList').component('actorList', {
             $scope.$on("searchTermChanged", function (event, searchTerm) {
                 if (searchTerm['sectionType'] == 'ActorList') {
                     self.actors = [];
+                    self.dynamicItems.reset();
                     self.searchTerm = searchTerm['searchTerm'];
                     self.searchField = searchTerm['searchField'];
-                    self.nextPage(0);
+                    // self.nextPage(0);
+                    self.dynamicItems.nextPage(0, false)
                 }
 
 
@@ -170,8 +316,10 @@ angular.module('actorList').component('actorList', {
                 if (runnerUp['sectionType'] == 'ActorList') {
                     console.log("Sort Order Changed!");
                     self.actors = [];
+                    self.dynamicItems.reset();
                     self.runnerUp = runnerUp['runnerUp'];
-                    self.nextPage(0);
+                    // self.nextPage(0);
+                    self.dynamicItems.nextPage(0, false)
                 }
 
             });
